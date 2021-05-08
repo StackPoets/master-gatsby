2c06b37f70cb07ee38f799c9b05b0bac3aacd6e8
Kenya Sullivan <kenya@stackpoets.com>
2021-05-08 10:38:52 -0400

    Add Pages

---
+++ gatsby/src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FourOFourPage () {
+    return (
+        <div>
+            <p>ooops wrong page.</p>
+        </div>
+    )
+}
---
+++ gatsby/src/pages/slickmasters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SlickmastersPage () {
+    return (
+        <div>
+            <p>Hey! I'm the Slickmasters page.</p>
+        </div>
+    )
+}
